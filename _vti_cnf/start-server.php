ab87c153b7fdef69e4268d1cba0274f73b58d0fe
Michael Tigue <mtigue@web4uonline.com>
2013-02-04 16:34:36 -0500

    1

---
+++ _vti_cnf/start-server.php
@@ -0,0 +1,22 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timelastmodified:TR|04 Feb 2013 19:44:40 -0000
+vti_timecreated:TR|20 Jan 2013 00:20:40 -0000
+vti_title:SR|Schantz Insurance Agency
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|04 Feb 2013 19:44:39 -0000
+vti_donotpublish:BW|true
+vti_cacheddtm:TX|04 Feb 2013 19:44:40 -0000
+vti_filesize:IR|1234
+vti_cachedtitle:SR|Schantz Insurance Agency
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|Q|css/jquery.mobile-1.2.0.min.css S|js/jquery-1.8.2.min.js S|js/jquery.mobile-1.2.0.min.js Q|css/web4u.css S|img/bg.png H|first.html H|prepare.html H|guide.html
+vti_cachedsvcrellinks:VX|FQUS|10/css/jquery.mobile-1.2.0.min.css FSUS|10/js/jquery-1.8.2.min.js FSUS|10/js/jquery.mobile-1.2.0.min.js FQUS|10/css/web4u.css FSUS|10/img/bg.png NHUS|10/first.html NHUS|10/prepare.html NHUS|10/guide.html
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|viewport width=device-width,\\ initial-scale=1
+vti_charset:SR|utf-8
