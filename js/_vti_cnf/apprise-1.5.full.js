05517c32e96bb5e7558e2e06001219f5f5c86998
Michael Tigue <mtigue@web4uonline.com>
2013-02-05 12:53:01 -0500

    16

--- js/_vti_cnf/apprise-1.5.full.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_author:SR|web4u\\mtigue
-vti_modifiedby:SR|web4u\\mtigue
-vti_timelastmodified:TR|04 Feb 2013 22:08:39 -0000
-vti_timecreated:TR|04 Feb 2013 22:08:39 -0000
-vti_cacheddtm:TX|04 Feb 2013 22:08:39 -0000
-vti_filesize:IR|3550
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|10/index.html
